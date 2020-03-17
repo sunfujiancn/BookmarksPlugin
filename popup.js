bf4febe429873320bdfcb8562b0c254747039ca5
sunfujian <sunfujiancn@gmail.com>
2020-03-17 20:51:22 +0800

    增加网络请求权限

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 $(function($){
   $("#syncBK").click(function(){
     chrome.bookmarks.getTree(function(results){
-      alert(JSON.stringify(results));
+      $.post("http://localhost:8080/save", {"params": results[0].children}, function (data) {
+        console.log(JSON.stringify(results));
+      });
     });
   });
 });
